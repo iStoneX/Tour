3a0158f217b3a153dca7f40b5d9268c68a6867a1
Stone Flynn <StoneFlynn72@gmail.com>
2023-07-19 04:36:11 -0700

    Created Vector Class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <stdexcept>
+class Vector{
+	public:
+		Vector() :data{nullptr}, sz{0}{};
+		Vector(int s) :data{new double[s]}, sz{s}{
+			for(int i=0;i!=sz;i++){
+				data[i] = i;
+			}
+		} 
+		double& operator[](int index){
+			if(index<0 || index>=sz)
+				throw std::out_of_range("Out of bounds stupid");
 
+			return data[index];
+		}
+		~Vector(){delete[] data;}
+	private:
+		double* data;
+		int sz;
+};
 
 int main(){
-
+	
 		std::cout << "Big Brain2\n";
+		Vector vec(3);
+		std::cout << vec[3] << '\n';
 }
