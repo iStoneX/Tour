949a5b80dd2bab3d7a5694840bfeca8db61215bd
Stone Flynn <stoneflynn72@gmail.com>
2023-07-29 10:20:40 -0700

    add and commit

--- main.cpp
+++ main.cpp
@@ -1,28 +1,9 @@
-#include <iostream>
-#include <stdexcept>
-class Vector{
-	public:
-		Vector() :data{nullptr}, sz{0}{};
-		Vector(int s) :data{new double[s]}, sz{s}{
-			for(int i=0;i!=sz;i++){
-				data[i] = i;
-			}
-		} 
-		double& operator[](int index){
-			if(index<0 || index>=sz)
-				throw std::out_of_range("Out of bounds stupid");
-
-			return data[index];
-		}
-		~Vector(){delete[] data;}
-	private:
-		double* data;
-		int sz;
-};
-//nice
-int main(){
-	
-		std::cout << "Big Brain\n";
-		Vector vec(3);
-		std::cout << vec[2] << '\n';
+#include<stdio.h>
+int main()
+{
+    static int i=5;
+    if(--i){
+        main();
+        printf("%d ",i);
+    }  
 }
